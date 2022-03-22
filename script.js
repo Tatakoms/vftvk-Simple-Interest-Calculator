e0c5f4bea5c239e5777d36e9d155dabdf59b1550
Tatakoms <87388160+Tatakoms@users.noreply.github.com>
2022-03-22 16:23:41 -0400

    1
    Background

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+window.onload = function compute(){
+  var button = document.getElementById("calc");
+  button.onclick = calculateInterest;
+
 }
-        
+function calculateInterest(){
+  var principalElement  = document.getElementById("principal");
+  var rateElement = document.getElementById("rate");
+  var timeElement = document.getElementById("time");
+  var resultText = document.getElementById("result");
+  var resultRow = document.getElementById("resultRow");
+  var principal = principalElement.value;
+  var rate = rateElement.value;
+  var time = timeElement.value;
+
+  if (isNaN(parseInt(principal)) || isNaN(parseInt(rate)) || isNaN(parseInt(time))) {
+    resultRow.style.display = "none";
+    var allInputs = document.getElementsByTagName('input');
+    for(i = 0; i < allInputs.length; i++){
+      if(allInputs[i].value == ""){
+        allInputs[i].value = "Empty!";
+      }
+    }
+  }
+  else{
+    resultRow.style.display = "block";
+    resultText.innerHTML = "The simple interest is " + ((parseInt(principal)*parseInt(rate)*parseInt(time))/100);
+  }
+
+}
